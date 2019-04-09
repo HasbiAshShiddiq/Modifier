5543a6d0551fbf4f6921e12670adb629412ce883
HasbiAshShiddiq <abi.hasbiashsiddiq@gmail.com>
2019-04-09 14:49:38 +0700

    Modifier

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat instan Class / object
+            Time objTime = new Time(2019, 10, 10, 10, 30, 40);
+
+            /*Memanggil method pada class tersebut
+            objTime.DisplayCurrentTime();*/
+
+            /*memberi nilai property
+            objTime.Year = 2000;
+            objTime.Month = 5;
+            objTime.Date = 1;*/
+
+            //Mengambil Nilai Dengan Property
+            Console.WriteLine("Year : {0}", objTime.Year.ToString());
+            Console.WriteLine("Month : {0}", objTime.Month.ToString());
+            Console.WriteLine("Date : {0}", objTime.Date);
+            Console.WriteLine("Hour : {0}", objTime.Hour);
+            Console.WriteLine("Minute : {0}", objTime.Minute);
+            Console.WriteLine("Second : {0}", objTime.Second);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifier
+{
+    class Time
+    {
+        /*Constructor
+        public Time()
+        {
+            Year = 2019;
+            Month = 10;
+            Date = 10;
+            Hour = 10;
+            Minute = 30;
+            Second = 40;
+        }*/
+        //Overload constructor
+        public Time(int year, int month, int date, int hour, int minute,
+            int second)
+        {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        
+        //Private Variables Atau Field
+        private int year;
+        private int month;
+        private int date;
+        private int hour;
+        private int minute;
+        private int second;
+        /*Properties
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Date { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }*/
+        public int Year {
+            get { return year; }
+            set { year = value; }
+        }
+        public int Month {
+            get { return month; }
+            set { month = value; }
+        }
+        public int Date {
+            get { return date; }
+            set { date = value; }
+        }
+        public int Hour {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Minute {
+            get { return minute; }
+            set { minute = value; }
+        }
+        public int Second {
+            get { return second; }
+            set { second = value; }
+        }
+
+        /*public void DisplayCurrentTime()
+        {
+            Console.WriteLine("waktu Sekarang : {0}", DateTime.Now.ToString());
+        }*/
+    }
+}
